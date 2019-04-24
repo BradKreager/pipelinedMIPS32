cfc84f8d28bddd947b74683c5ebc0a0333912a36
Grant Haack <granthaack@protonmail.com>
2019-04-24 04:26:03 -0700

    started work on factorial TB

--- MIPS32.srcs/sim_1/new/tb_fact.v
+++ MIPS32.srcs/sim_1/new/tb_fact.v
@@ -20,10 +20,50 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb_fact(
-
+module tb_fact();
+    
+    reg [1:0]a_t;
+    reg we_t;
+    reg [3:0]wd_t;
+    reg rst_t;
+    reg clk_t;
+    
+    wire [31:0]rd_t;
+    
+    fact_top DUT(
+        .a(a_t),
+        .we(we_t),
+        .wd(wd_t),
+        .rst(rst_t),
+        .clk(clk_t),
+        .rd(rd_t)
     );
     
-    fact_top DUT();
+    task tick;
+      begin
+        clk_t = 1'b0;
+        #5;
+        clk_t = 1'b1;
+        #5;
+      end
+    endtask
     
+    task reset;
+      begin
+        rst_t = 1'b0;
+        #5;
+        rst_t = 1'b1;
+        #5;
+        tick;
+        rst_t = 1'b0;
+      end
+    endtask
+    
+    integer errors = 0;
+    integer test_value = 32'h0;
+    
+    initial begin
+        reset;
+        tick;
+    end
 endmodule
